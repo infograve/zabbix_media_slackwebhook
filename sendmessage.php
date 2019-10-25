a26977e2cdc0e9e192c1525c4c723463288ac7c3
K.Imamura <ko1@infograve.jp>
2019-10-25 19:28:04 +0900

    fieldsが書き出されない。

--- sendmessage.php
+++ sendmessage.php
@@ -1,24 +1,68 @@
 #!/usr/bin/php
 
 <?php
-/* ########## config ########## */
-$urlSlackWebhook = 'https://hooks.slack.com/services/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';
-
 /* ********** Get Argument ********** */
 $argument = array(0=>'',1=>'',2=>'',3=>'');
 if($argc>0) $argument = $argv + $argument;
 
 /* ********** Create Payload ********** */
+$messageColor = 'danger';
+if(preg_match('/^OK:/',$argument[2])) $color = 'good';
+
+$messageFields = array();
+$tmpLine = explode("\n",$argument[3]);
+for($count=0,$max=count($tmpLine); $count<$max; $count++){
+  $tmpField = explode(': ',$tmpLine[count]);
+  array_push($messageFields, array($tmpField[0]=>$tmpField[1]));
+}
+
 $message = array(
-  'text' => $argument[2]
+  'attachments' => array(
+    array(
+      'fallback' => $argument[2],
+      'color' => $messageColor,
+      'pretext' => '*'.$argument[2].'*',
+      'fields' => json_encode($messageFields)
+    )
+  )
 );
+
+/*
+"attachments": [
+        {
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "#36a64f",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment",
+            "fields": [
+                {
+                    "title": "Priority",
+                    "value": "High",
+                    "short": false
+                }
+            ],
+            "image_url": "http://my-website.com/path/to/image.jpg",
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
+        }
+    ]
+
+  'text' => '*'.$argument[2].'*'."\n".
+*/
 
 $message = json_encode($message);
 $payload = 'payload='.urlencode($message);
 
 /* ********** Send to SlackWebhook ********** */
 $handlerCURL = curl_init();
-curl_setopt($handlerCURL,CURLOPT_URL,$urlSlackWebhook);
+curl_setopt($handlerCURL,CURLOPT_URL,$argument[1]);
 curl_setopt($handlerCURL,CURLOPT_POST,true);
 curl_setopt($handlerCURL,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($handlerCURL,CURLOPT_TIMEOUT,10);
