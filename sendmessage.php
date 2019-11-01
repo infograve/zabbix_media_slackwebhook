3598ae91c9130916005b2a8a351557f49ca550c6
K.Imamura <ko1@infograve.jp>
2019-11-01 16:58:46 +0900

    とりあえず、目論見通りの動作。

--- sendmessage.php
+++ sendmessage.php
@@ -7,13 +7,14 @@ if($argc>0) $argument = $argv + $argument;
 
 /* ********** Create Payload ********** */
 $messageColor = 'danger';
-if(preg_match('/^OK:/',$argument[2])) $color = 'good';
+if(preg_match('/^OK:/',$argument[2])) $messageColor = 'good';
 
 $messageFields = array();
-$tmpLine = explode("\n",$argument[3]);
-for($count=0,$max=count($tmpLine); $count<$max; $count++){
-  $tmpField = explode(': ',$tmpLine[count]);
-  array_push($messageFields, array($tmpField[0]=>$tmpField[1]));
+$tmpMessage = explode("\n",$argument[3]);
+
+for($count=0,$max=count($tmpMessage);$count<$max;$count++){
+  $tmpLine = explode(':::',$tmpMessage[$count]);
+  array_push($messageFields, array('title'=>$tmpLine[0],'value'=>$tmpLine[1],'short'=>'true'));
 }
 
 $message = array(
@@ -22,40 +23,10 @@ $message = array(
       'fallback' => $argument[2],
       'color' => $messageColor,
       'pretext' => '*'.$argument[2].'*',
-      'fields' => json_encode($messageFields)
+      'fields' => $messageFields
     )
   )
 );
-
-/*
-"attachments": [
-        {
-            "fallback": "Required plain-text summary of the attachment.",
-            "color": "#36a64f",
-            "pretext": "Optional text that appears above the attachment block",
-            "author_name": "Bobby Tables",
-            "author_link": "http://flickr.com/bobby/",
-            "author_icon": "http://flickr.com/icons/bobby.jpg",
-            "title": "Slack API Documentation",
-            "title_link": "https://api.slack.com/",
-            "text": "Optional text that appears within the attachment",
-            "fields": [
-                {
-                    "title": "Priority",
-                    "value": "High",
-                    "short": false
-                }
-            ],
-            "image_url": "http://my-website.com/path/to/image.jpg",
-            "thumb_url": "http://example.com/path/to/thumb.png",
-            "footer": "Slack API",
-            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-            "ts": 123456789
-        }
-    ]
-
-  'text' => '*'.$argument[2].'*'."\n".
-*/
 
 $message = json_encode($message);
 $payload = 'payload='.urlencode($message);
